d3da2c2272cdd45ecbc914536c234f030283d084
Mikolaj <mikoajmicht@gmail.com>
2025-07-28 19:04:38 +0200

    feat: react 19.1.1 app,tailwind setup + dockerfile

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="bg-blue-500 text-white p-4">
+        Hello, Tailwind!
+      </div>
+  );
+}
+
+export default App;
